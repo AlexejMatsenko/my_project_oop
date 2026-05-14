877bb2c8f232d3c70712a9fa27b22ef893275a93
Alexej Matsenko <matsenko.alexej@yandex.ru>
2026-05-14 11:36:43 +0300

    Обновлены функции

--- src/products.py
+++ src/products.py
@@ -9,8 +9,25 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, price):
+        if price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        self.__price = price
+
+
+
+    @classmethod
+    def new_product(cls, product: dict):
+        return cls(product["name"], product["description"], product["price"], product["quantity"])
 
 
 class Category(Product):
@@ -25,6 +42,19 @@ class Category(Product):
     def __init__(self, name, description, products=None) -> None:
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+
+    def add_product(self, product):
+        self.__products.append(product)
+        self.product_count += 1
+
+    @property
+    def products(self):
+        product_str = ""
+        for product in self.__products:
+            product_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return product_str
+
